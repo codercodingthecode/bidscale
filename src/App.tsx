0cbc02543da1ff847471084d040329476f8f4e34
Andy Lavor <andersonlavor@gmail.com>
2022-08-23 18:39:24 -0500

    Add Project Base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {AlertExample} from "./Alert/AlertExample";
+import {Container, Grid, Typography} from "@mui/material";
+import {AlertManager} from "./Alert/AlertManager";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container maxWidth={'xs'} >
+          <Grid container spacing={3} justifyContent={'center'} pt={8}>
+            <Grid item xs={12} >
+                <Typography variant={'h4'} color={'primary'} align={'center'}>Alert Example</Typography>
+            </Grid>
+              <Grid item xs={12}>
+                  <AlertManager />
+                  <AlertExample />
+            </Grid>
+          </Grid>
+      </Container>
   );
 }
 
